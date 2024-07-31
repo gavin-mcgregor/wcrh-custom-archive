88cb247b7c87f41a5ccff2edf6d094796e582eac
Gavin <gurven-mckruger@hotmail.co.uk>
2024-07-31 15:18:24 +0100

    first commit

---
+++ build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => 'ff03436081a5257477a3');
---
+++ build/view.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => 'd7631efcd63ce98ae3f7');
